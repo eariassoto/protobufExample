2f167cbbe658fb51b46f9a78db12da8e11ec6bd4
Emmanuel Arias Soto <ariassotoemmanuel@gmail.com>
2018-09-20 09:24:03 -0500

    commit files

---
+++ ProtoBufCs/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using Google.Protobuf;
+using Google.Protobuf.Examples.AddressBook;
+using static Google.Protobuf.Examples.AddressBook.Person.Types;
+
+
+namespace ProtoBufCs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Usage: ProtoBufCs.txt ADDRESS_BOOK_FILE\n");
+                return;
+            }
+            
+            Person john = new Person
+            {
+                Id = 1234,
+                Name = "John Doe",
+                Email = "jdoe@example.com",
+                Phones = { new PhoneNumber { Number = "555-4321", Type = PhoneType.Home } }
+            };
+
+            AddressBook book = new AddressBook
+            {
+                People = { john }
+            };
+            using (var output = File.Create(args[0]))
+            {
+                book.WriteTo(output);
+            }
+        }
+    }
+}
